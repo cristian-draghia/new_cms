bb16835100dccace5f88f109c1d43d43207404c9
cristian-draghia <draghiacristian97@gmail.com>
2021-05-09 15:00:23 +0300

    Finish section12

---
+++ includes/db.php
@@ -0,0 +1,23 @@
+<?php 
+
+$db = array(
+  'db_host' => 'localhost',
+  'db_user' => 'root',
+  'db_pass' => '',
+  'db_name' => 'cms',
+);
+
+foreach ( $db as $key => $value ) {
+
+  define (strtoupper($key), $value);
+
+}
+
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if ( $connection ) {
+  // echo "We are connected";
+}
+
+?>
